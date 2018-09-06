caa4a199a452c09a6dfb3ad1a70ab8a524e37f9e
lovewenyo <931977674@qq.com>
2018-09-06 19:38:05 +0800

    优化了代码

--- src/pers/dingjie/classic/BubbleSort.java
+++ src/pers/dingjie/classic/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
     /**
      * @description 传统的冒泡排序
      */
-    public void bubbleSort_1(int []arr) {
+    public void bubbleSort_1(int[] arr) {
 	    int n = arr.length;
 	    
 	    for(int i = 0 ; i< n-1; ++i) {//控制循环趟数
@@ -28,7 +28,7 @@ public class BubbleSort {
      *	设置一标志性变量pos,用于记录每趟排序中最后一次进行交换的位置。
      *	如果进行某一趟排序时并没有进行数据交换，则说明数据已经按要求排列好，可立即结束排序，减少比较次数
      */
-    public void bubbleSort_2(int []arr){
+    public void bubbleSort_2(int[] arr){
         int i= arr.length - 1;  //初始时,最后位置保持不变
         
         while (i > 0) {
@@ -51,7 +51,7 @@ public class BubbleSort {
      * 	我们考虑利用在每趟排序中进行正向和反向两遍冒泡的方法一次可以得到两个最终值(最大者和最小者) 
      * 	从而使排序趟数几乎减少了一半。
      */
-    public void bubbleSort_3(int []arr){
+    public void bubbleSort_3(int[] arr){
         int low = 0;
         int high= arr.length - 1; //设置变量的初始值
         int temp,j;
@@ -80,7 +80,7 @@ public class BubbleSort {
      * @description 鸡尾酒排序
      * 	上面两种排序的结合版	
      */
-    public void bubbleSort_4(int []arr){
+    public void bubbleSort_4(int[] arr){
     	int high = arr.length - 1;
     	int temp,j,low = 0;
     	boolean swapped = true;
