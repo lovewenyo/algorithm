026ad0e6fa4134132938fa9985dc1adf15e2814f
jaceding <931977674@qq.com>
2021-05-26 11:55:08 +0800

    feat: 日常刷题

--- src/main/java/per/jaceding/algorithm/leetcode/dp/Jz_42.java
+++ src/main/java/per/jaceding/algorithm/leetcode/dp/Jz_42.java
@@ -14,7 +14,7 @@ package per.jaceding.algorithm.leetcode.dp;
  * @author jaceding
  * @date 2021/5/21
  */
-public class JzOffer_42 {
+public class Jz_42 {
 
     public int maxSubArray(int[] nums) {
         int n = nums.length;
@@ -55,7 +55,7 @@ public class JzOffer_42 {
 
     public static void main(String[] args) {
         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(new JzOffer_42().maxSubArray(arr));
+        System.out.println(new Jz_42().maxSubArray(arr));
     }
 
 }
---
+++ src/main/java/per/jaceding/algorithm/leetcode/dp/Jz_62.java
@@ -0,0 +1,24 @@
+package per.jaceding.algorithm.leetcode.dp;
+
+/**
+ * 假设最后剩下的数字为x,每次删除第m个元素，每一轮数字x，index1表示倒数第一轮数字x所处的位置
+ * 倒数第1轮，index1=0
+ * 倒数第2轮，index2=(index1 + m) % 2
+ * 倒数第3轮，index3=(index2 + m) % 3
+ * 倒数第4轮，index4=(index3 + m) % 4
+ * @author jaceding
+ * @date 2021/5/26
+ */
+public class Jz_62 {
+
+    public int lastRemaining(int n, int m) {
+        if(n == 1){
+            return 0;
+        }
+        int index = 0;
+        for (int i = 2; i <= n; i++) {
+            index = (index + m) % i;
+        }
+        return index;
+    }
+}
