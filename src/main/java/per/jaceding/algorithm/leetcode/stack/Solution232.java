d7c3ac9442c6ea466d7a7b6fea1d8110f9a2d0e1
jaceding <931977674@qq.com>
2021-06-08 19:42:10 +0800

    feat: 日常刷题

--- src/main/java/per/jaceding/algorithm/leetcode/stack/Solution232.java
+++ src/main/java/per/jaceding/algorithm/leetcode/stack/Solution232.java
@@ -4,38 +4,51 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
+ * 用栈实现队列
+ *
  * @author jaceding
  * @date 2021/6/8
  */
 public class Solution232 {
 
-    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> stack1 = new ArrayDeque<>();
+    Deque<Integer> stack2 = new ArrayDeque<>();
 
     /**
      * Push element x to the back of queue.
      */
     public void push(int x) {
-        stack.push(x);
+        stack1.push(x);
     }
 
     /**
      * Removes the element from in front of queue and returns that element.
      */
     public int pop() {
-        return stack.poll();
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
     }
 
     /**
      * Get the front element.
      */
     public int peek() {
-        return stack.peek();
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
     }
 
     /**
      * Returns whether the queue is empty.
      */
     public boolean empty() {
-        return stack.isEmpty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
