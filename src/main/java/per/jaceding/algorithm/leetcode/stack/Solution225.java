a7f82513aff74387635f0b1b31c32e03cfdc6d2a
jaceding <931977674@qq.com>
2021-06-08 19:55:58 +0800

    feat: 日常刷题

--- src/main/java/per/jaceding/algorithm/leetcode/stack/Solution225.java
+++ src/main/java/per/jaceding/algorithm/leetcode/stack/Solution225.java
@@ -5,38 +5,55 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
+ * 用队列实现栈
+ *
  * @author jaceding
  * @date 2021/6/8
  */
 public class Solution225 {
 
-    Deque<Integer> queue = new ArrayDeque<>();
+    Deque<Integer> queue1 = new ArrayDeque<>();
+    Deque<Integer> queue2 = new ArrayDeque<>();
+
+    boolean isQueue1 = true;
 
     /**
      * Push element x onto stack.
      */
     public void push(int x) {
-        queue.push(x);
+        if (isQueue1) {
+            queue1.add(x);
+            while (!queue2.isEmpty()) {
+                queue1.add(queue2.poll());
+            }
+            isQueue1 = false;
+        } else {
+            queue2.add(x);
+            while (!queue1.isEmpty()) {
+                queue2.add(queue1.poll());
+            }
+            isQueue1 = true;
+        }
     }
 
     /**
      * Removes the element on top of the stack and returns that element.
      */
     public int pop() {
-        return queue.pop();
+        return isQueue1 ? queue2.poll() : queue1.poll();
     }
 
     /**
      * Get the top element.
      */
     public int top() {
-        return queue.peek();
+        return isQueue1 ? queue2.peek() : queue1.poll();
     }
 
     /**
      * Returns whether the stack is empty.
      */
     public boolean empty() {
-        return queue.isEmpty();
+        return isQueue1 ? queue2.isEmpty() : queue1.isEmpty();
     }
 }
