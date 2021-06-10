acf967c46c582e18148b75468e7e01153dfcd13e
jaceding <931977674@qq.com>
2021-06-10 13:47:50 +0800

    feat: 日常刷题

---
+++ src/main/java/per/jaceding/algorithm/leetcode/heap/Jz_40.java
@@ -0,0 +1,31 @@
+package per.jaceding.algorithm.leetcode.heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author jaceding
+ * @date 2021/6/10
+ */
+public class Jz_40 {
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        int n = arr.length;
+        if (k == 0 || n == 0) {
+            return new int[]{};
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k, (o1, o2) -> o2 - o1);
+        for (int i = 0; i < n; i++) {
+            if (heap.size() < k) {
+                heap.add(arr[i]);
+            } else if (arr[i] < heap.peek()) {
+                heap.poll();
+                heap.add(arr[i]);
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[n - 1 - i] = heap.poll();
+        }
+        return res;
+    }
+}
