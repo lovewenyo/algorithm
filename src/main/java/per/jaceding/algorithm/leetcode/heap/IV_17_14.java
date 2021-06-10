136a40266f67fa7563fcd891469e76c08122839b
jaceding <931977674@qq.com>
2021-06-10 13:57:04 +0800

    feat: 日常刷题

--- src/main/java/per/jaceding/algorithm/leetcode/heap/IV_17_14.java
+++ src/main/java/per/jaceding/algorithm/leetcode/heap/IV_17_14.java
@@ -1,16 +1,15 @@
 package per.jaceding.algorithm.leetcode.heap;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 /**
- * 最大堆 解决TOP K问题（非最优解）
- *
  * @author jaceding
  * @date 2021/6/10
  */
-public class Jz_40 {
+public class IV_17_14 {
 
-    public int[] getLeastNumbers(int[] arr, int k) {
+    public int[] smallestK(int[] arr, int k) {
         int n = arr.length;
         if (k == 0 || n == 0) {
             return new int[]{};
@@ -29,5 +28,11 @@ public class Jz_40 {
             res[i] = heap.poll();
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 2, 4, 6, 8};
+        int k = 4;
+        System.out.println(Arrays.toString(new IV_17_14().smallestK(arr, k)));
     }
 }
