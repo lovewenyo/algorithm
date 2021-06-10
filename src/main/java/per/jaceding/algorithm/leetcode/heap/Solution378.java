ffa2b1cc453ad277ef2a5c127db8749d0cab97ba
jaceding <931977674@qq.com>
2021-06-10 20:06:24 +0800

    feat: 日常刷题

---
+++ src/main/java/per/jaceding/algorithm/leetcode/heap/Solution378.java
@@ -0,0 +1,40 @@
+package per.jaceding.algorithm.leetcode.heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author jaceding
+ * @date 2021/6/10
+ */
+public class Solution378 {
+
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int i = 0, j = 0;
+        int maxI = n - 1, maxJ = n - 1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, (o1, o2) -> o2 - o1);
+        while (i <= maxI && j <= maxJ) {
+            if (queue.size() < k) {
+                queue.add(matrix[i][j]);
+            } else if (matrix[i][j] < queue.peek()) {
+                queue.poll();
+                queue.add(matrix[i][j]);
+            } else {
+                maxJ = j;
+            }
+            if (++j > maxJ) {
+                j = 0;
+                i++;
+            }
+        }
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {-5}
+        };
+        int k = 1;
+        System.out.println(new Solution378().kthSmallest(arr, k));
+    }
+}
