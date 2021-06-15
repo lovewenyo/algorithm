b9427bdc13fbd0d7fc8e2b82a97319481356731d
jaceding <931977674@qq.com>
2021-06-15 11:34:18 +0800

    feat: 日常刷题

---
+++ src/main/java/per/jaceding/algorithm/leetcode/bit/Solution338.java
@@ -0,0 +1,19 @@
+package per.jaceding.algorithm.leetcode.bit;
+
+/**
+ * @author jaceding
+ * @date 2021/6/15
+ */
+public class Solution338 {
+
+    public int[] countBits(int n) {
+        if (n < 0) {
+            return new int[]{};
+        }
+        int[] arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = Integer.bitCount(i);
+        }
+        return arr;
+    }
+}
